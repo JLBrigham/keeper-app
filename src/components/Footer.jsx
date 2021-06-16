e38d6ce6b2682a656a3ce1d4d0dc4b66702cd11a
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-06-16 16:05:35 +0200

    Note compoenent added and imported to App

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import React from "react"
 function Footer() {
     const currentYear = new Date().getFullYear()
 
-return (<p>Copyright {currentYear}</p>)
+return (<footer><p>Copyright {currentYear}</p></footer>)
 }
 
 export default Footer;
---
+++ src/components/Note.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Note() {
+    return <div className="note">
+        <h1>Write note here</h1><p>This is my first note</p>
+    </div>
+}
+
+export default Note;
