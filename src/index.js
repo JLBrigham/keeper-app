b56ca984493840e7c7c2934907fbb9044433cac3
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-06-16 14:34:54 +0200

    Created App component

--- src/index.js
+++ src/index.js
@@ -10,3 +10,7 @@
 //https://l1pp6.csb.app/
 
 //HINT: You will need to study the classes in teh styles.css file to appy styling.
+
+import React from "react"
+import ReactDom from "react-dom"
+import App from "./components/app"
